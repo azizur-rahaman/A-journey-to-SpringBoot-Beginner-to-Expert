f8d85a3a43bab3bcec87e4c530d7e1a5a3df32a9
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2025-07-15 18:54:22 +0600

    JPA Entitles & JPA Relationship

---
+++ spring-boot/[1]spring-6-webapp/src/main/java/aziz/springframework/spring6webapp/domain/Book.java
@@ -0,0 +1,27 @@
+package aziz.springframework.spring6webapp.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_books", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
