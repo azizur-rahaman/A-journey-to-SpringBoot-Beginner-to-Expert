a703df3908d3b91c05c3d011d486c0400fb274b5
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2025-07-16 11:16:23 +0600

    [22] Initialize spring-boot with data

--- spring-boot/[1]spring-6-webapp/src/main/java/aziz/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ spring-boot/[1]spring-6-webapp/src/main/java/aziz/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -4,7 +4,9 @@ import aziz.springframework.spring6webapp.domain.Book;
 import aziz.springframework.spring6webapp.repositories.*;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
@@ -21,11 +23,41 @@ public class BootstrapData implements CommandLineRunner {
         eric.setFirstName("Eric");
         eric.setLastName("Sam");
 
+        Author rod = new Author();
+        rod.setFirstName("Rod");
+        rod.setLastName("Johnson");
+
+        Book noEJB = new Book();
+        noEJB.setTitle("J2EE Development without EJB");
+        noEJB.setIsbn("123456");
+
         Book ddd = new Book();
         ddd.setTitle("Domain driven design");
         ddd.setIsbn("123456789");
 
+
         Author ericsaved = authorRepository.save(eric);
         Book dddSave = bookRepository.save(ddd);
+        Author rodSaved = authorRepository.save(rod);
+        Book noEJBSaved = bookRepository.save(noEJB);
+
+        // Establish bidirectional relationships
+        ericsaved.getBooks().add(dddSave);
+        dddSave.getAuthors().add(ericsaved);
+        
+        rodSaved.getBooks().add(noEJBSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
+
+        // Save the updates
+        authorRepository.save(ericsaved);
+        authorRepository.save(rodSaved);
+        bookRepository.save(dddSave);
+        bookRepository.save(noEJBSaved);
+
+        System.out.println("In Bootstrap");
+        System.out.println("Author count: " + authorRepository.count());
+        System.out.println("Book count: " + bookRepository.count());
+
+        
     }
 }
