39ad7fe0833427a6f27e1891f493a5c67c04f112
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2025-07-17 12:28:51 +0600

    23. Configuring Publisher Relationships

--- spring-boot/[1]spring-6-webapp/src/main/java/aziz/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ spring-boot/[1]spring-6-webapp/src/main/java/aziz/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -1,6 +1,7 @@
 package aziz.springframework.spring6webapp.bootstrap;
 import aziz.springframework.spring6webapp.domain.Author;
 import aziz.springframework.spring6webapp.domain.Book;
+import aziz.springframework.spring6webapp.domain.Publisher;
 import aziz.springframework.spring6webapp.repositories.*;
 
 import org.springframework.boot.CommandLineRunner;
@@ -11,14 +12,22 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        // Check if data already exists to avoid duplicate insertion
+        if (authorRepository.count() > 0) {
+            System.out.println("Data already exists, skipping bootstrap");
+            return;
+        }
+
         Author eric = new Author();
         eric.setFirstName("Eric");
         eric.setLastName("Sam");
@@ -35,7 +44,16 @@ public class BootstrapData implements CommandLineRunner {
         ddd.setTitle("Domain driven design");
         ddd.setIsbn("123456789");
 
+        // Create publisher first
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("My Publisher");
+        publisher.setAddress("123 Main Street");
+        Publisher savedPublisher = publisherRepository.save(publisher);
 
+        // Set publisher before saving books
+        ddd.setPublisher(savedPublisher);
+
+        // Save entities first
         Author ericsaved = authorRepository.save(eric);
         Book dddSave = bookRepository.save(ddd);
         Author rodSaved = authorRepository.save(rod);
@@ -48,7 +66,7 @@ public class BootstrapData implements CommandLineRunner {
         rodSaved.getBooks().add(noEJBSaved);
         noEJBSaved.getAuthors().add(rodSaved);
 
-        // Save the updates
+        // Save the updates (only once)
         authorRepository.save(ericsaved);
         authorRepository.save(rodSaved);
         bookRepository.save(dddSave);
@@ -57,6 +75,7 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("In Bootstrap");
         System.out.println("Author count: " + authorRepository.count());
         System.out.println("Book count: " + bookRepository.count());
+        System.out.println("Publisher count: " + publisherRepository.count());
 
         
     }
